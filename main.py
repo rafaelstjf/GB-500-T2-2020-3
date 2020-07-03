cf0e253787d9813410548cb6879662bf5c7fcfd8
Rafael Terra <rafaelstjf@hotmail.com>
2020-07-03 20:39:33 -0300

    seeds dataset funcionando

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import numpy as np  # multidimensional arrays
 import math
 import matplotlib.pyplot as plt
-import matplotlib.axes as ax
 from sklearn.cluster import KMeans  # Kmeans algorithm from scikit-learning
 
-
 def open_file():
-    file_name = 'wine.data'
-    header_name = 'wine_header.data'
+    file_name = 'seeds_dataset.csv'
+    header_name = 'seeds_dataset_header.csv'
     deli = ','
     data_elements = np.genfromtxt(file_name, delimiter=deli)
     data_header = np.genfromtxt(header_name, delimiter=deli, dtype=str)
     data_file = (data_elements, data_header)
     return data_file
 
-
-def plot_all_raw_data(data_file):
-    print(data_file[0])
-    for i in range (1, len(data_file[1])):
-        for j in range (i+1, len(data_file[1])):
-            chart = plt.scatter(x=data_file[0][:, i], y=data_file[0][:, j], c=data_file[0][:, 0])
-            plt.xlabel(data_file[1][i])
-            plt.ylabel(data_file[1][j])
-            plt.title("Raw data")
-            plt.legend(*chart.legend_elements(), loc="upper right", title="Classes")
-            plt.savefig("raw_data" + str(i) + "_" + str(j) + ".png")
-
 def plot_raw_data(data_file, x_axys, y_axys):
-    chart = plt.scatter(x=data_file[0][:, x_axys], y=data_file[0][:, y_axys], c=data_file[0][:, 0])
+    chart = plt.scatter(x=data_file[0][:, x_axys], y=data_file[0][:, y_axys], c=data_file[0][:, 7])
     plt.xlabel(data_file[1][x_axys])
     plt.ylabel(data_file[1][y_axys])
     plt.title("Raw data")
@@ -40,16 +26,29 @@ def plot_result_data(data_file, result, x_axys, y_axys):
     plt.ylabel(data_file[1][y_axys])
     plt.title("Result")
     plt.legend(*chart.legend_elements(), loc="upper right", title="Classes")
-    #plt.axes().add_artist(legend1)
     plt.show()
-
-""" def plot_another_chart(data_file, result):
-    a = []
-    for i in range (0, len(result)):
-        a.append(i)
-    plt.plot(a, data_file[0][:,0],c='red')
-    plt.scatter(a, result)
-    plt.show() """
+def standardisation(data_file):
+    # scikit can do it using
+    # from sklearn import preprocessing
+    # preprocessing.scale(data_file[0])
+    # but i've already done it so =)
+    average = []
+    for i in range(np.size(data_file[0], 1)):
+        average.append(np.sum(data_file[0][:, i]))
+    for i in range(np.size(data_file[0], 1)):
+        average[i] /= np.size(data_file[0], 0)
+    standard_dev = []
+    for i in range(np.size(data_file[0], 1)):
+        sum = 0
+        for j in range(np.size(data_file[0], 0)):
+            sum+= ((data_file[0][j, i])-average[i])**2
+        standard_dev.append(math.sqrt((sum/np.size(data_file[0], 0))))
+    fixed = data_file
+    for i in range(np.size(data_file[0], 1)):
+        for j in range(np.size(data_file[0], 0)):
+            fixed[0][j, i] = (data_file[0][j, i]-average[i])/standard_dev[i]
+    return fixed
+    
 def run_kmeans(data_file, max_clusters=10):
     # creates the instance of kmeans object
     results = []
@@ -80,12 +79,14 @@ def run_kmeans(data_file, max_clusters=10):
     print("Optimal number of clusters: " + str(optimal_num))
     return results[optimal_index]
 
-
-raw_data = open_file()
-data = (raw_data[0][:, 1:], raw_data[1][1:])
-plot_raw_data(raw_data, 1, 2)
-result = run_kmeans(data, 8)
-result+=1
-plot_result_data(data, result, 0, 1)
-# plot_another_chart(raw_data, result)
-print(result)
+def main():
+    raw_data = open_file()
+    #plot_all_raw_data(raw_data)
+    data = (raw_data[0][:, :7], raw_data[1][:7]) # the seeds dataset's last column is the class
+    plot_raw_data(raw_data, 0, 4)
+    standard_data = standardisation(data)
+    result = run_kmeans(standard_data, 8)
+    result+=1
+    plot_result_data(data, result, 0, 1)
+    #print(result)
+main()
